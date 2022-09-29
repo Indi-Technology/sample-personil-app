3c7666ebfa594fdb0405ac32744d77c65e33b69b
jefri_yh <jefri_yh@yahoo.co.id>
2022-09-29 15:25:32 +0700

    add rank

---
+++ app/src/main/java/com/mabes/projectakhir/RankResponse.kt
@@ -0,0 +1,28 @@
+package com.mabes.projectakhir
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class RankResponse(
+
+	@field:SerializedName("data")
+	val data: List<DataItemRank>
+) : Parcelable
+
+@Parcelize
+data class DataItemRank(
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String,
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("created_at")
+	val createdAt: String,
+
+	@field:SerializedName("id")
+	val id: Int
+) : Parcelable
