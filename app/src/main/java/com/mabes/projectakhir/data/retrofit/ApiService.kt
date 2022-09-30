48cb9f877b12592d8f0bb6cdc8ad7934c10a3f76
jefri_yh <jefri_yh@yahoo.co.id>
2022-09-30 11:12:00 +0700

    modify adapter

--- app/src/main/java/com/mabes/projectakhir/data/retrofit/ApiService.kt
+++ app/src/main/java/com/mabes/projectakhir/data/retrofit/ApiService.kt
@@ -1,13 +1,14 @@
 package com.mabes.projectakhir.data.response.retrofit
 
-import com.mabes.projectakhir.ListPersonilResponse
+
+import com.mabes.projectakhir.ListUserResponse
 import com.mabes.projectakhir.RankResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("api/pers")
-    fun getUserList():Call<ListPersonilResponse>
+    fun getUserList():Call<ListUserResponse>
 
     @GET("api/ranks")
     fun getUserRank():Call<RankResponse>
--- app/src/main/java/com/mabes/projectakhir/ui/main/MainActivity.kt
+++ app/src/main/java/com/mabes/projectakhir/ui/main/MainActivity.kt
@@ -5,67 +5,53 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.mabes.projectakhir.ui.addedit.AddEditActivity
-import com.mabes.projectakhir.DataItem
-import com.mabes.projectakhir.ListPersonilResponse
-import com.mabes.projectakhir.data.response.retrofit.ApiConfig
+
+import com.mabes.projectakhir.DataUser
+
 import com.mabes.projectakhir.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var mainActivityBinding: ActivityMainBinding
+    private lateinit var listUserAdapter: ListUserAdapter
+    private lateinit var btnSearch:ImageButton
+    private val mainViewModel:MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBinding.root)
+       showRecyclerView()
 
-        viewBinding.listUserRv.setHasFixedSize(true)
+        mainViewModel.listUser.observe(this){
+            setUserData(it)
+        }
 
-        enqueueApi()
-    }
-
-    @SuppressLint("LongLogTag")
-    private fun showRecyclerView(dataItem: List<DataItem>){
-        viewBinding.listUserRv.layoutManager = LinearLayoutManager(
-            this@MainActivity)
-
-        val adapter = ListUserAdapter(dataItem)
-
-        viewBinding.listUserRv.adapter = adapter
-
-        viewBinding.listUserAddFab.setOnClickListener {
-            val intent = Intent(this, AddEditActivity::class.java)
-            startActivity(intent)
+        mainViewModel.isLoading.observe(this){
+            showLoading(it)
         }
 
     }
 
-    private fun enqueueApi(){
-        val client = ApiConfig.getApiService().getUserList()
-        client.enqueue(object : Callback<ListPersonilResponse>{
-            override fun onResponse(
-                call: Call<ListPersonilResponse>,
-                response: Response<ListPersonilResponse>
-            ) {
-                if (response.isSuccessful){
-                      if (response.body() != null){
-                        showRecyclerView(response!!.body()!!.data)
-                      }
-                } else {
-                    Log.e("ON RESPONSE FAILURE",
-                        "onResponse: ${response.message()}"
-                    )
-                }
-            }
+    private fun showRecyclerView(){
+        mainActivityBinding.rvUser.layoutManager = LinearLayoutManager(
+            this@MainActivity)
 
-            override fun onFailure(call: Call<ListPersonilResponse>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-
-        })
     }
+
+    private fun setUserData(itemUser:List<DataUser>){
+        listUserAdapter.setListUser(itemUser)
+        mainActivityBinding.rvUser.adapter =listUserAdapter
+    }
+
+    private fun showLoading(isLoading:Boolean){
+        mainActivityBinding.loadRv.visibility=
+            if (isLoading) View.VISIBLE else View.GONE
+    }
+
+
 }
